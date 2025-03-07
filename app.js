bcb902b99262438ba270da259d498319c716cdc8
darshil-nakrani <darshilnakrani296@gmail.com>
2025-03-07 13:38:57 +0530

    Finally solve img problem

--- app.js
+++ app.js
@@ -103,7 +103,7 @@ app.use(
                 "'self'",
                 "blob:",
                 "data:",
-                "https://res.cloudinary.com/douqbebwk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://res.cloudinary.com/dgqbxe3cw/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
                 "https://images.unsplash.com/",
             ],
             fontSrc: ["'self'", ...fontSrcUrls],
